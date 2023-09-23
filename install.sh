74339752d829343abff7e8968966d4d690ce52d3
David P. Chassin <david.chassin@me.com>
2023-09-23 16:37:09 -0700

    Update install.sh
    Signed-off-by: David P. Chassin <david.chassin@me.com>

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
+REPONAME=arras-energy/module_template
+TOOLNAME=template
 mkdir -p /usr/local/opt/gridlabd/current/src/module
-git clone https://arras.energy/module_template template
-cd template/source
+git clone https://github.com/$REPONAME $TOOLNAME -b $BRANCH --depth 1
+cd $TOOLNAME/source
 echo "make install" | gridlabd shell
