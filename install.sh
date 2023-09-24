1ae1b7109ff52b18f6950861c9a31eba6387d643
David P. Chassin <david.chassin@me.com>
2023-09-24 10:10:04 -0700

    Update install.sh
    Signed-off-by: David P. Chassin <david.chassin@me.com>

--- install.sh
+++ install.sh
@@ -1,5 +1,2 @@
-export GRIDLABD=$(gridlabd --version=install)
-mkdir -p ${GRIDLABD}/src/module
-git clone https://github.com/${REPO} ${GRIDLABD}/src/module/${NAME} -b ${BRANCH:-master} --depth 1
-cd ${GRIDLABD}/src/module/${NAME}/source
-echo "make install" | gridlabd shell
+echo 'git clone https://github.com/${REPO} ${GLD_SRC}/module/${NAME} -b ${BRANCH:-master} --depth 1' | gridlabd shell
+echo 'cd ${GLD_SRC}/module/${NAME}/source;make install' | gridlabd shell
