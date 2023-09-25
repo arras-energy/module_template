6dfa4aa0ac0f4c4323a011e3aa7daa9897ac8936
dchassin <dchassin@slac.stanford.edu>
2023-09-25 07:28:45 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-echo 'git clone https://github.com/${REPO} ${GLD_SRC}/module/${NAME} -b ${BRANCH:-master} --depth 1' | gridlabd shell
+echo 'git clone -sL https://github.com/${REPO} ${GLD_SRC}/module/${NAME} -b ${BRANCH:-master} --depth 1' | gridlabd shell
 echo 'cd ${GLD_SRC}/module/${NAME}/source;make install' | gridlabd shell
